2e54c4ed456d5ca499a92d76af38f44d94598568
kjkim <apio1130@naver.com>
2025-09-30 00:11:49 +0900

    feat: vitest 테스트 코드 실행되도록 수정

--- vue-blog-project/src/components/__tests__/HelloWorld.spec.ts
+++ vue-blog-project/src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,19 @@
-import { describe, it, expect } from 'vitest'
+import {describe, it, expect, vi} from 'vitest'
 
-import { mount } from '@vue/test-utils'
+import {mount} from '@vue/test-utils'
+import {createTestingPinia} from '@pinia/testing'
 import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(HelloWorld, {
+      props: {msg: 'Hello Vitest'},
+      global: {
+        plugins: [createTestingPinia({
+          createSpy: vi.fn,
+        })],
+      }
+    })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
